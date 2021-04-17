34aaa55835b42f871900fdc7e20c629719369d64
Brian Garrity <briangarrity@Brians-MacBook-Air.local>
2021-04-17 19:56:33 -0400

    JavaWorld

---
+++ JavaWorld/src/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, Welcome to JavaWorld!");
+        System.out.println("This is my first project using Java!!");
+    }
+}
